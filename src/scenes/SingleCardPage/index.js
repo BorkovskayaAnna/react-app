172c1c26d45be5c0854924222ce22b32bb8bd597
BorkovskayaAnna <borkovskayaa23@gmail.com>
2024-02-15 17:15:24 +0200

    console log

--- src/scenes/SingleCardPage/index.js
+++ src/scenes/SingleCardPage/index.js
@@ -2,18 +2,18 @@ import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { fetchSingleData } from '../../api'
 import { SingleCardItem } from '../../components/SingleCardItem'
-// import { ErrorPage} from '../../scenes/ErrorPage'
+import { ErrorPage} from '../../scenes/ErrorPage'
 import { Loading } from '../../components/Loading'
 
 export const SingleCardPage = () => {
 	const { id } = useParams();
   const [card, setCard] = useState([]);
-  // const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(true)
 
 	useEffect(() => {
     fetchSingleData(id)
     .then(data => setCard(data))
-    // .finally(() => setLoading(false))
+    .finally(() => setLoading(false))
   }, [card.id, id])
 
   if(!card.id) {
@@ -25,7 +25,8 @@ export const SingleCardPage = () => {
   return (
     <>
     {card.id === id
-    && <SingleCardItem card={card} />
+    ? <SingleCardItem card={card} />
+    : <ErrorPage />
     }
     </>
   )
