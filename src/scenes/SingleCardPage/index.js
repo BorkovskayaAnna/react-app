a2e650ad4321351edb114ea57320fc573a2849c0
BorkovskayaAnna <borkovskayaa23@gmail.com>
2024-02-15 17:50:07 +0200

    console log

--- src/scenes/SingleCardPage/index.js
+++ src/scenes/SingleCardPage/index.js
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { fetchSingleData } from '../../api'
 import { SingleCardItem } from '../../components/SingleCardItem'
-import { ErrorPage} from '../../scenes/ErrorPage'
-import { Loading } from '../../components/Loading'
+// import { ErrorPage} from '../../scenes/ErrorPage'
+// import { Loading } from '../../components/Loading'
 
 export const SingleCardPage = () => {
 	const { id } = useParams();
